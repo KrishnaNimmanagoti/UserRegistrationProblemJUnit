a4d19ee6eb8d8fb006e91d990c96a9d232863a63
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-03-03 17:36:20 +0530

    Developed for Test mood

---
+++ src/main/java/com/bridgelabz/exceptionHandling/MoodAnalyser.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.exceptionHandling;
+
+public class MoodAnalyser {
+
+    public String analyseMood(String message){
+
+        if (message.contains("sad")){
+
+            return "SAD";
+
+        }
+        else {
+
+            return "HAPPY";
+
+        }
+
+    }
+
+}
---
+++ src/test/java/MoodAnalyserTest.java
@@ -0,0 +1,23 @@
+import com.bridgelabz.exceptionHandling.MoodAnalyser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyserTest {
+
+    MoodAnalyser mood;
+
+    @BeforeEach
+    public void objectMethod(){
+
+        mood = new MoodAnalyser();
+
+    }
+
+    @Test
+    public void testSadMood(){
+
+        System.out.println(mood.analyseMood("I am in sad mood"));
+
+    }
+
+}
