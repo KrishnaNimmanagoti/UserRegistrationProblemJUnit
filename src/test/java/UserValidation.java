0c9f2d3c16dbfefafadcdace8a8733cb4ce149d6
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-03-04 05:08:10 +0530

    Refactored to validate all Email ID's

--- src/test/java/UserValidation.java
+++ src/test/java/UserValidation.java
@@ -7,6 +7,7 @@ public class UserValidation {
 
     UserRegistration user;
     boolean result;
+    String[] allEmails;
 
     @BeforeEach
     public void setup(){
@@ -42,7 +43,7 @@ public class UserValidation {
 
     @Test
     public void testEmailHappy(){
-        result = user.checkEmail("Krishna@gmail.com");
+        result = user.checkEmail("abc111@abc.com");
         System.out.println(result);
         Assertions.assertTrue(result);
     }
@@ -68,7 +69,7 @@ public class UserValidation {
 
     @Test
     public void testPasswordHappy() {
-        result = user.checkPassword("krishna123AS@asD");
+        result = user.checkPassword("abc@yahoo.com");
         System.out.println(result);
         Assertions.assertTrue(result);
     }
@@ -77,6 +78,40 @@ public class UserValidation {
         result = user.checkPassword("krishna123A@@SasD");
         System.out.println(result);
         Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testAllEmailsHappy() {
+
+         allEmails = new String[]{"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+                                    "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
+                                        "abc+100@gmail.com"};
+
+//        "abc@.com.my", "abc@gmail.com.aa.au", ".abc@com.com", "abc+100@gmail.com"
+
+        for (String emailIDs: allEmails) {
+
+            result = user.checkEmail(emailIDs);
+            System.out.println(result);
+            Assertions.assertTrue(result);
+
+        }
+    }
+
+    @Test
+    public void testAllInValidEmails() {
+
+        allEmails = new String[]{"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
+                                    ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com",
+                                    "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+
+        for (String emailIDs: allEmails) {
+
+            result = user.checkEmail(emailIDs);
+            System.out.println(result);
+            Assertions.assertFalse(result);
+
+        }
     }
 
 }
