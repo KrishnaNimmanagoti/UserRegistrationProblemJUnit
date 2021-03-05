e99bab1e95753d21826388ef5d980e0c59e75763
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-03-05 21:14:45 +0530

    Refactored to throw Custom Exception

--- src/test/java/UserValidation.java
+++ src/test/java/UserValidation.java
@@ -1,5 +1,4 @@
 import com.bridgelabz.uservalidation.UserRegistration;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,91 +14,99 @@ public class UserValidation {
         user = new UserRegistration();
 
     }
+
     @Test
     public void testFirstNameHappy(){
+
         result = user.checkFirstName("Krishna");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+        user.checkException(result);
+
     }
     @Test
     public void testFirstNameSad(){
+
         result = user.checkFirstName("Kr");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+        user.checkException(result);
+
     }
 
     @Test
-    public void testLastNameHappy(){
+     public void testLastNameHappy() {
+
         result = user.checkLastName("Krishna");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+        user.checkException(result);
+
     }
     @Test
     public void testLastNameSad(){
+
         result = user.checkLastName("Kr");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+        user.checkException(result);
+
     }
 
     @Test
     public void testEmailHappy(){
+
         result = user.checkEmail("abc111@abc.com");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+        user.checkException(result);
+
     }
     @Test
     public void testEmailSad(){
+
         result = user.checkEmail("Krishnagmail.com");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+        user.checkException(result);
+
     }
 
     @Test
     public void testMobileNumHappy() {
+
         result = user.checkMobileNum("7569918165");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+        user.checkException(result);
+
     }
     @Test
     public void testMobileNumSad() {
+
         result = user.checkMobileNum("007569918165");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+        user.checkException(result);
+
     }
 
     @Test
     public void testPasswordHappy() {
-        result = user.checkPassword("abc@yahoo.com");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+
+        result = user.checkPassword("Krishna@123");
+        user.checkException(result);
+
     }
     @Test
     public void testPasswordSad() {
+
         result = user.checkPassword("krishna123A@@SasD");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+        user.checkException(result);
+
     }
 
     @Test
+
     public void testAllEmailsHappy() {
 
-         allEmails = new String[]{"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
-                                    "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
-                                        "abc+100@gmail.com"};
+        allEmails = new String[]{"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+                "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
+                "abc+100@gmail.com"};
 
-//        "abc@.com.my", "abc@gmail.com.aa.au", ".abc@com.com", "abc+100@gmail.com"
-
-        for (String emailIDs: allEmails) {
+        for (String emailIDs : allEmails) {
 
             result = user.checkEmail(emailIDs);
-            System.out.println(result);
-            Assertions.assertTrue(result);
+            user.checkException(result);
 
         }
     }
-
     @Test
-    public void testAllInValidEmails() {
+    public void testAllInValidEmailsSad() {
 
         allEmails = new String[]{"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
                                     ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com",
@@ -108,8 +115,7 @@ public class UserValidation {
         for (String emailIDs: allEmails) {
 
             result = user.checkEmail(emailIDs);
-            System.out.println(result);
-            Assertions.assertFalse(result);
+            user.checkException(result);
 
         }
     }
